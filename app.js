d897af5b1ead346a87bd14ea6b225097b34463aa
TrinhThuong2001 <trinhvanthuong20012000@gmail.com>
2023-04-18 13:55:49 +0700

    Synchronous and Asynchronous programming

--- app.js
+++ app.js
@@ -8,7 +8,19 @@
 
 // app.listen(3000)
 
-const fs = require('fs')
-const fileName = "target.txt"
+const fs = require("fs");
+const fileName = "target.txt";
 
-fs.watch(fileName, () => console.log(`File changed`));
+const data = fs.readFileSync(fileName);
+console.log("SYNCHRONOUS", data.toString());
+
+// fs.watch(fileName, () => console.log(`File changed`));
+
+fs.readFile(fileName, (err, data) =>{
+    if (err){
+        console.log(err);
+    }
+    console.log("ASYNCHRONOUS", data.toString());
+});
+
+console.log("NORMAL TEXT");
